4da1d699e54c5991237098f06b07376a1bc607f5
Dexter <dexterfonghy@gmail.com>
2021-01-02 01:37:40 +0800

    Update logging

--- api/telegram_api.py
+++ api/telegram_api.py
@@ -9,7 +9,7 @@ bot = telebot.TeleBot(TELEGRAM_API_TOKEN)
 
 # Send a message to Telegram chat without options
 def send_message(user: User, state, session_id, response):
-    print("Sending response '{}' for user {} session {} intent '{}'"
+    print("Sending response '{}' for user {} session {} state '{}'"
           .format(response, user.id, session_id, state))
 
     return bot.send_message(user.id, response)
@@ -17,7 +17,7 @@ def send_message(user: User, state, session_id, response):
 
 # Send a message to Telegram chat with options, with two options in a row by default
 def send_message_with_options(user: User, state, session_id, response, *options, row_width=2):
-    print("Sending response '{}' with options '{}' for user {} session {} intent '{}'"
+    print("Sending response '{}' with options '{}' for user {} session {} state '{}'"
           .format(response, options, user.id, session_id, state))
 
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=row_width)
